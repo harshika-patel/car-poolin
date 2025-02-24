100374f63279500247f26d1cfa1f88b4a25dcb21
Harshika Patel <harshikapatel2574@gmail.com>
2025-02-23 22:54:38 -0500

    conflict solve

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 import './App.scss'
 import { AuthProvider } from './contexts/AuthContext';
@@ -33,45 +32,10 @@ function App() {
       </Routes>
       </BrowserRouter>
       </AuthProvider>
-=======
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
->>>>>>> b5c2a7bed85c0e2df2ae95c9017d7dc10a14da24
     </>
   )
 }
 
 export default App
-<<<<<<< HEAD
 
      
-=======
->>>>>>> b5c2a7bed85c0e2df2ae95c9017d7dc10a14da24
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-<<<<<<< HEAD
 import './index.scss'
-=======
-import './index.css'
->>>>>>> b5c2a7bed85c0e2df2ae95c9017d7dc10a14da24
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
